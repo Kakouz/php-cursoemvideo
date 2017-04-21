bf498901f0b1105c2b4452b1ea7bf80f40f18e69
Tiago <tf.prudente@gmail.com>
2017-04-20 22:48:39 -0300

    Primeiro exemplo em PHP
    Primeiro exemplo em PHP, o famoso olá mundo!

---
+++ Aula 3/olamundo/olamundo.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Primeiro exemplo</title>
+    <style>
+      h2 {
+        color: #80a2ff;
+        text-shadow: 1px 1px 1px black;
+      }
+    </style>
+  </head>
+  <body>
+    <h1>Testando PHP</h1>
+    <?php
+    echo "<h2>Olá, <br />Mundo!</h2>";
+    ?>
+  </body>
+</html>
